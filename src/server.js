fe6eb6426687b3b387776a7e21eb44077fccb29f
Ronit <maity.ronit18@gmail.com>
2025-12-31 00:50:46 +0530

    allow cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import app from "./app.js";
+import cors from "cors";
 
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
