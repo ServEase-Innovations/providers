4778af4a392fb8628547d7ce230f8bbccd68e2a3
Ronit <maity.ronit18@gmail.com>
2025-12-31 16:29:01 +0530

    update cors

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./swagger/swagger.js";
 import providerRoutes from "./routes/provider.routes.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
